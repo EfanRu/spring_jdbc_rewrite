60b9187cee2a443a4a20c48956e4ba54c6b8ae04
slava <ffgeniy@mail.ru>
2019-10-09 17:37:51 +0300

    Edit JdbcRunner.java

--- src/main/java/ru/rewrite/sprig_jdbc/util/JdbcRunner.java
+++ src/main/java/ru/rewrite/sprig_jdbc/util/JdbcRunner.java
@@ -4,20 +4,20 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.jdbc.core.JdbcTemplate;
 import ru.rewrite.sprig_jdbc.DAO.BookDaoImpl;
+import ru.rewrite.sprig_jdbc.model.Book;
 
 public class JdbcRunner {
     public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext("");
         //!!!need add xml config
-        BookDaoImpl bookDao = context.getBean("BookDao");
+        BookDaoImpl bookDao = (BookDaoImpl) context.getBean("BookDao");
+
+        bookDao.createBook("Clean code", 12.33, "Robert Martin", 22);
+        bookDao.createBook("SQL", 13.88, "Kris Fraily", 17);
+        bookDao.createBook("Patterns", 2.33, null, 2);
+
+        for (Book b : bookDao.listBooks()) {
+            System.out.println(b.toString());
+        }
     }
-
-    /*
-            ApplicationContext context =
-                new ClassPathXmlApplicationContext("jdbctemplate-developer-config.xml");
-
-        JdbcTemplateDeveloperDaoImpl jdbcTemplateDeveloperDao =
-                (JdbcTemplateDeveloperDaoImpl) context.getBean("jdbcTemplateDeveloperDao");
-
-    */
 }
